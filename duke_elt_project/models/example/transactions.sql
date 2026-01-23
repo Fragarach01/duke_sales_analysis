11753de7a7e31cd882cf30d56e34e4c15be0f3ff
Fragarach01 <107695984+Fragarach01@users.noreply.github.com>
2026-01-23 15:03:11 +0000

    updated transactions file in dbt

--- duke_elt_project/models/example/transactions.sql
+++ duke_elt_project/models/example/transactions.sql
@@ -2,24 +2,20 @@
 
 select 
     date::DATE,
-    time::TIME, 
-    order_no::NUMERIC, 
-    CASE 
-        WHEN account_no IN ('""', NULL) THEN 1 
-        ELSE account_no::INTEGER 
-    END AS account_no,  
+    time::TIME,
     account_id::NUMERIC, 
+    terminal,
     type, 
     description, 
     detail, 
     information, 
+    employee,
     CASE 
         WHEN quantity IN ('""', NULL) THEN 1 
         ELSE quantity::INTEGER 
     END AS quantity, 
     sales_amount::FLOAT, 
     discount::FLOAT, 
-    VAT::FLOAT, 
     payment_amount::FLOAT, 
     change::FLOAT
 from {{ source('target_db', 'transactions') }}
